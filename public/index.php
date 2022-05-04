5f6e599400a3d9a75ef7f2eece81bbd12dcd57b4
Dimka-Prog <91527667+Dimka-Prog@users.noreply.github.com>
2022-05-05 00:37:10 +1000

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -11,12 +11,16 @@ spl_autoload_register(function ($class){
 });
 
 use Subdirectory\subClass1;
+use Subdirectory\SubdirectoryLevel2\subLevel2Class1 as Level2;
 
 $value1 = new Class1();
-echo $value1->printMessage(Class1::class) . ": " . $GLOBALS['path'] . "<br>";
+echo $value1->printMessage(Class1::class) . ": " . $GLOBALS['path'] . "<br>" . PHP_EOL;
 
 $value2 = new subClass1();
-echo $value2->printMessage(subClass1::class) . ": " . $GLOBALS['path'];
+echo $value2->printMessage(subClass1::class) . ": " . $GLOBALS['path'] . "<br>" . PHP_EOL;
+
+$value3 = new Level2();
+echo $value3->printMessage(Level2::class) . ": " . $GLOBALS['path'] . PHP_EOL;
 
 
 
---
+++ src/Subdirectory/SubdirectoryLevel2/subLevel2Class1.php
@@ -0,0 +1,9 @@
+<?php
+    namespace Subdirectory\SubdirectoryLevel2;
+    class subLevel2Class1
+    {
+        function printMessage($typeClass)
+        {
+            echo "Вызвался $typeClass" . PHP_EOL;
+        }
+    }
